e7af861d240af1b34a9718d3f9f07ec3e0ac0717
Jack13-Han <2250266@ecc.ac.jp>
2025-05-08 16:06:46 +0900

    NEw

--- JKad06S1.java
+++ JKad06S1.java
@@ -1,6 +1,13 @@
 public class JKad06S1 {
     public static void main(String[] args) {
-        
+        int x =1;
+
+        while (x<10) { 
+           
+            System.out.println(x+"\t" + (x*2) +"\t" +(x*3)+"\t" +(x*4)+"\t" +(x*5)+"\t" +(x*6)+"\t" +(x*7)+"\t" +(x*8)+"\t" +(x*9) );
+            x+=1;
+            
+        }
     }
     
 }
