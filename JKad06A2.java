d70a54c50e6aa97dc0246d91891273fdf7acc882
Jack13-Han <2250266@ecc.ac.jp>
2025-05-08 15:35:21 +0900

    New

--- JKad06A2.java
+++ JKad06A2.java
@@ -1,6 +1,20 @@
+
+import java.util.*;
+
 public class JKad06A2 {
     public static void main(String[] args) {
-        
+        Scanner in = new Scanner(System.in);
+
+        System.out.print("9 桁までの正の整数を入力してください>");
+        int x= in.nextInt();
+
+        String z= "1";
+        while (x>1) { 
+           int y = x%10;
+           x/=10;
+            System.out.println(z+" の位は"+y);
+            z+=0;
+        }
     }
     
 }
