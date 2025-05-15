942a5b523d1c60ebc9bdc9d57b0de4e9a5eb0706
Jack13-Han <2250266@ecc.ac.jp>
2025-05-15 12:20:59 +0900

    New!

--- Jkad07/src/JKad07X.java
+++ Jkad07/src/JKad07X.java
@@ -10,31 +10,25 @@ public class JKad07X {
         int ball = ran.nextInt(100);    //    ၀ ကနေ ၁၀၀ အထိ Randon တွေထုတ်မယ်
         int y = 1;
         int x= -1;  //x ကို -1 ပေးရချင်းမှာ ၀ နဲ့ မညီချင်လို့ ပေးထားတာဖြစ်ပါတယ်
+        int min =0;
+        int max = 99;
 
-        System.out.print(y+" 回目0 から99 までの数値を入力してください＞>");
+
         while (ball != x) {  //ball က ထ နဲ့ မညီရင် Loop အမြဲတမ်ဖြစ်နေမယ်
-
+            System.out.print(y+" 回目"+min+" から"+max+" までの数値を入力してください＞>");
             x = in.nextInt();
-
-            if (ball < x) {
-                System.out.println(x+"より小さいです！");
-                System.out.print(y+" 回目0 から"+ball+" までの数値を入力してください＞>");
-                if (ball< x){
-                    int num1= in.nextInt();
-                    System.out.println();
-                    System.out.print(y+" 回目"+num1+" から"+ball+" までの数値を入力してください＞>");
+            if (x>=min && x<=max){
+                if (x>ball){
+                    System.out.println(x+"より小さいです！");
+                    max=x-1;
                 }
+                if (x<ball){
+                    System.out.println(x+"より大きいです！");
+                    min=x+1;
+                }
+                y++;
             }
-            if(ball > x){
-                System.out.println(x+"より大きいです！");
-                System.out.print(y+" 回目0 から"+ball+" までの数値を入力してください＞>");
-                System.out.print(y+" 回目0 から"+ball+" までの数値を入力してください＞>");
-            }
-            if (ball ==x ) {
-                System.out.println("正解しました！");
-            }
-
-            y++;
         }
+        System.out.println("正解しました！");
     }
 }
